e2d8df30238e7f4ad70f3d32b973d3046192de0b
Ana Caldeira <anavsscaldeira@gmail.com>
2023-11-27 16:38:59 -0300

    add new template from web

--- mainProject/website/admin.py
+++ mainProject/website/admin.py
@@ -1,10 +1,15 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from .models import Post
 
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'subtitle', 'catogories'] 
+    list_display = ['title', 'subtitle', 'catogories', 'approved']
 
+    def get_queryset(self, request):
+        return Post.objects.all() # pode filtrar se quiser
 
 admin.site.register(Post, PostAdmin)
