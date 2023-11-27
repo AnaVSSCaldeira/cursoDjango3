fc548d6f03a0861cfe278b6ce2039cbc2d964395
Ana Caldeira <anavsscaldeira@gmail.com>
2023-11-27 15:42:10 -0300

    new fields

--- mainProject/website/models.py
+++ mainProject/website/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
+class Categories(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     subtitle = models.CharField(max_length=20)
     content = models.TextField()
+    catogories = models.CharField(
+        max_length = 2,
+        choices = Categories.choices,
+        default = Categories.GR,
+    )
+
+    approved = models.BooleanField(default = True)
+
+    def __str__(self):
+        return self.title
--- mainProject/website/views.py
+++ mainProject/website/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def hello_test(request):
     gamesList = ['Bioshock Infinite', 'The Great Ace Attorney Chronicles', 'Barony', 'Slay The Spire', 'Dont Starve Together']
-    data = {'name': 'Deusana', 'gamesList': gamesList}
+    postList = Post.objects.all()
+    data = {
+        'name': 'Deusana',
+        'gamesList': gamesList,
+        'postList': postList
+        }    
+
     return render(request, 'index.html', data)
     # return HttpResponse("Welcome to the website!")
